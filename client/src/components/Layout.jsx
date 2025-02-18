02b2e4c4d39f5a89837d76a459a42e07f012a35b
Kapoen <kjasomers@gmail.com>
2025-02-18 17:05:54 +0100

    Made navbar layout.

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 export default function Layout({ children }) {
     return (
         <main>
-            <nav>
-                HOME
+            <nav className="bg-delft-blue p-4 mb-8">
+                <ul className="flex space-x-8 justify-center text-center text-xl">
+                    <li>
+                        <NavLink to="/" className={({ isActive }) => `
+                            ${isActive ? "text-vanilla font-bold" : "text-ghost-white-dark"}
+                        `}
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/addSong" className={({ isActive }) => `
+                            ${isActive ? "text-vanilla font-bold" : "text-ghost-white-dark"}
+                        `}
+                        >
+                            Add song
+                        </NavLink>
+                    </li>
+                </ul>
             </nav>
             {children}
         </main>
