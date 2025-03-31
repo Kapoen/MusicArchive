20ac3c45c9052b32e8e87ffad58f960e893155b0
Kapoen <kjasomers@gmail.com>
2025-03-31 11:50:23 +0200

    Changed api call

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://kapoen.github.io/MusicArchive/",
+    baseURL: "https://musicarchive.onrender.com",
     headers: {
         "Content-Type": "application/json"
     }
